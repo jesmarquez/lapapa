5aab75cf5808e9ab6347c108719886eb01642610
DESKTOP-5G35IDM\Jesus Marquez <jesmqz@gmail.com>
2023-03-31 14:10:51 -0400

    Set API dev and refactor FoodCard

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ import backgroundMain from './assets/background-cook-2.jpg'
 import './App.css'
 import { FoodCard } from './components/FoodCard';
 import { OrdenarModal } from './components/OrdenarModal';
+import { useFetch } from "./hooks/useFetch";
 
 function App() {
 
+  const { data, isLoading, hasError } = useFetch('http://localhost:8000/foods');
+
+  // console.log(data);
   const [showOrdenar, setShowOrdenar] = useState(false);
 
   const handleOrdenarClose = () => setShowOrdenar( false );
@@ -15,12 +19,12 @@ function App() {
 
   let urlVegetables = 'https://firebasestorage.googleapis.com/v0/b/lapapa-faabf.appspot.com/o/vegetables.jpg?alt=media&token=46648d09-d8bc-41eb-8983-75faf22180e1';
 
-  let detailsFood = {
-    urlImage : 'https://firebasestorage.googleapis.com/v0/b/lapapa-faabf.appspot.com/o/arepa-queso-carne-mechada.jpg?alt=media&token=1a85b5d8-3858-439a-bc58-8d26af1a6b7d',
-    foodName: 'Arepa queso y carne',
-    calories: '900 cals',
-    precio : '8.00'
-  };
+  // let detailsFood = {
+  //   urlImage : 'https://firebasestorage.googleapis.com/v0/b/lapapa-faabf.appspot.com/o/arepa-queso-carne-mechada.jpg?alt=media&token=1a85b5d8-3858-439a-bc58-8d26af1a6b7d',
+  //   foodName: 'Arepa queso y carne',
+  //   calories: '900 cals',
+  //   precio : '8.00'
+  // };
 
   // console.log(detailsFood);
   return (
@@ -28,7 +32,6 @@ function App() {
 
       <div style={{ backgroundImage: "url(" + urlVegetables + ")" }}>
         <div className="container-fluid pt-4 pr-4">
-
           <div className="row justify-content-md-center">
             <div className="col">
               <div className="bg-dark bg-gradient p-3 fixed-top">
@@ -36,29 +39,19 @@ function App() {
               </div>
             </div>
           </div>
-
-
-
-          <div className="row justify-content-md-center foodOne">
-            <div className="col-2"></div>
-            <div className="col">
-              <FoodCard setShowOrdenar = { handleOrdenarMostrar } detailsFood = { detailsFood }/>
-            </div>
-            <div className="col-2"></div>
-          </div>
-
-
-          <div className="row justify-content-md-center mt-3">
-            <div className="col-2"></div>
-            <div className="col">
-              <FoodCard setShowOrdenar = { handleOrdenarMostrar } detailsFood = { detailsFood }/>
-            </div>
-            <div className="col-2"></div>
-          </div>
-
+          {
+            data
+            ?
+            data.map( detailsFood => (
+              <FoodCard 
+                key={ detailsFood.id } 
+                setShowOrdenar = { handleOrdenarMostrar } 
+                detailsFood = { detailsFood }/>
+            ))
+            : <h1>Cargando!</h1>
+          }
           <OrdenarModal showOrdenar = { showOrdenar } handleOrdenarClose = { handleOrdenarClose }/>
         </div> {/* div container */}
-
       </div> {/* div app */}
 
      </>
--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -4,29 +4,31 @@ import arepaAmarilloCarne from '../assets/arepa-queso-carne-mechada.jpg';
 export const FoodCard = ( { setShowOrdenar, detailsFood } ) => {
 
   // console.log( detailsFood );
-
   const { urlImage, foodName, calories, precio } = detailsFood;
-
   // console.log( urlImage, name, calories, precio );
-
   // let urlImage = 'https://firebasestorage.googleapis.com/v0/b/lapapa-faabf.appspot.com/o/arepa-queso-carne-mechada.jpg?alt=media&token=1a85b5d8-3858-439a-bc58-8d26af1a6b7d';
   return (
-    <div className="card">
-        <img src={ urlImage } className="card-img-top" alt="arepa-amarillo-carne"></img>
-        <div className="card-body">
-            <h3 className="card-title">{ foodName }</h3>
-            <h5 className="card-title">{ calories }</h5>
-            <h2 className='text-end'>${ precio }</h2>
-            <p className="card-text">Hecho a base de masa de maíz seco molido o de harina de maíz precocida. Rellena con carne mechada y queso rayado gouda.</p>
-            <button className="btn btn-dark float-end"  data-bs-toggle="modal" data-bs-target="#ordenarModal" onClick={ setShowOrdenar } >Agregar</button>
-            <select className="form-select" aria-label="Default select example" style={{ width: '25%' }} defaultValue={'DEFAULT'}>
-              <option value="DEFAULT">Cantidad?</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-            </select>
+    <div className="row justify-content-md-center foodOne">
+      <div className="col-2"></div>
+      <div className="col">
+        <div className="card">
+            <img src={ urlImage } className="card-img-top" alt="arepa-amarillo-carne"></img>
+            <div className="card-body">
+                <h3 className="card-title">{ foodName }</h3>
+                <h5 className="card-title">{ calories }</h5>
+                <h2 className='text-end'>${ precio }</h2>
+                <p className="card-text">Hecho a base de masa de maíz seco molido o de harina de maíz precocida. Rellena con carne mechada y queso rayado gouda.</p>
+                <button className="btn btn-dark float-end"  data-bs-toggle="modal" data-bs-target="#ordenarModal" onClick={ setShowOrdenar } >Agregar</button>
+                <select className="form-select" aria-label="Default select example" style={{ width: '25%' }} defaultValue={'DEFAULT'}>
+                  <option value="DEFAULT">Cantidad?</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                </select>
+            </div>
         </div>
+      </div>
+      <div className="col-2"></div>
     </div>
-    
   )
 }
