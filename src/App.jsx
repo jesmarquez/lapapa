02f92452e1822f5404bbd33a5cca28623cf30587
DESKTOP-5G35IDM\Jesus Marquez <jesmqz@gmail.com>
2023-06-24 01:53:03 -0400

    Refactor deleteItemIntoCart function

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { Header } from './components/ui/Header'
 import { useFetch } from "./hooks/useFetch";
 import { Cart } from "./components/Cart";
 
-
 function App() {
   // state for show/hide cart (sidebar)
   const [showOffcanvas, setShowOffcanvas] = useState(false);
@@ -16,11 +15,8 @@ function App() {
   const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem('cart')) || []);
   const [amountCart, setAmount] = useState(JSON.parse(localStorage.getItem('amount')) || 0);
 
-
   const handleOrdenarClose = () => setShowOrdenar(false);
-
   const handleOrdenarMostrar = () => setShowOrdenar(true);
-
   const handleOnlyShowCart = () => setShowOffcanvas(true);
 
   const handleShowOffcanvas = (newFood = {}) => {
@@ -59,32 +55,23 @@ function App() {
   const handleHideOffcanvas = () => setShowOffcanvas(false);
 
   const deleteItemIntoCart = async ( id ) => {
-    console.log(cartItems);
     let amount=0;
-
-    let cartItemsLocalStorage = JSON.parse(localStorage.getItem('cart'));
-
-    const updateCartItemsLocalStorage = await cartItemsLocalStorage.filter( (item) => {
-      return item.id !== id;
-    });
-
-    localStorage.setItem('cart', JSON.stringify(updateCartItemsLocalStorage));
-
+ 
     const newCartItems = await cartItems.filter( (item) => {
       if (item.id === id) amount = item.amount;
       return item.id !== id;
     });
-    // console.log(newCartItems);
-    setCartItems(newCartItems);
+       
     const item = data.find(item => item.id === id);
     const itemPrice = parseFloat(item.precio);
-    // console.log(amount, itemPrice);
     const subtotalDelete = amount * itemPrice;
     // console.log(newTotal);
     const newTotal = amountCart - subtotalDelete;
+    setCartItems(newCartItems);
     setAmount(newTotal);
-    localStorage.setItem('amount', newTotal);
 
+    localStorage.setItem('amount', newTotal);
+    localStorage.setItem('cart', JSON.stringify(newCartItems));
     return;
   }
 
